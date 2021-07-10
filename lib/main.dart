724474ec8861a6f86f5010b99b4ffacc28220bc5
chetan-sawant <48862552+chetan-sawant@users.noreply.github.com>
2021-07-10 16:34:29 +0530

    first test commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.red,
         ),
         home: RootPage(),
     );
